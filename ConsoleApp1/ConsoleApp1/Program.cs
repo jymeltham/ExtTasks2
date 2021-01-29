36d4d823d1ab9df6b622c625659fb82843abe7e6
Miss Jay Muirhead <51444366+jymeltham@users.noreply.github.com>
2021-01-29 09:01:41 +0000

    Update Program.cs

--- ConsoleApp1/ConsoleApp1/Program.cs
+++ ConsoleApp1/ConsoleApp1/Program.cs
@@ -10,34 +10,13 @@ namespace ConsoleApp1
     {
         static void Main(string[] args)
         {
-            /* Task 1: Pangrams 
-             * 
-                The quick brown fox jumps over the lazy dog”; note how all 26 English-language letters are used in the sentence.
-                Your goal is to implement a program that takes a series of strings (one per line) and 
-                prints either True (the given string is a pangram), or False if it is not.
-            */
-
-
-
-
-            /* Task 2: Years in Range 
-             * 
-                Write a program to count the number years in a range that has a repeated digit.
-                For example, 2012 has a repeated digit, but 2013 does not.
-            */
-
-            /* Task 3: Of Mice and Men 
-            * 
-               Have the programme allow a user to play the “mice and men” game. The game works like this:
-                Randomly generate a 4-digit number. Ask the user to guess a 4-digit number. For every digit that the user guessed correctly 
-                in the correct place, they have a “mouse”. For every digit the user guessed correctly in the wrong place is a “man” 
-                Every time the user makes a guess, tell them how many “mice” and “men” they have.
-                Once the user guesses the correct number, the game is over. Keep track of the number of guesses the user makes throughout 
-                the game and tell the user at the end.
-            
-                Extension:
-                    Deal with “mice” and “mouse” and “man” and “men ” properly. 
-           */
+            /**Task
+               Name that Number!
+               Telephone Keypads often have letters associated with each number. 
+               This means that 0141 117 2556 could be stored as 0141 - CAT - DOOR.
+               Create a program that can convert a phone number with “letters” into one that only contains digits.
+               
+               EXT: Can you develop your program so that only words in the dictionary are allowed?*/
 
         }
     }
